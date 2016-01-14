5cf279b9a92ff593e4c84820679b121ceea833bd
= <geison.sn@gmail.com>
2016-01-14 10:46:59 -0400

    Aula 07 de Angular do Alura: exclusão de fotos

--- alura/Angular/alurapic/public/js/controllers/fotos-controller.js
+++ alura/Angular/alurapic/public/js/controllers/fotos-controller.js
@@ -2,6 +2,7 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
 
 	$scope.fotos = [];
 	$scope.filtro = "";
+	$scope.mensagem = "";
 
 	$http.get("v1/fotos")
 	.success(function(fotos) {
@@ -10,6 +11,19 @@ angular.module("alurapic").controller("FotosController", function($scope, $http)
 	.error(function(erro) {
 		console.log(erro);
 	});
+
+	$scope.remover = function(foto) {
+		$http.delete("v1/fotos/" + foto._id)
+		.success(function() {
+			$scope.mensagem = "Foto " + foto.titulo + " foi removida com sucesso";
+			var indiceFoto = $scope.fotos.indexOf(foto);
+			$scope.fotos.splice(indiceFoto, 1);
+		})
+		.error(function(erro) {
+			console.log(erro);
+			$scope.mensagem = "Não foi possível remover a foto " + foto.titulo;
+		});
+	}
 
 	/*
 	var promise = $http.get("v1/fotos");
